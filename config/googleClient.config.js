eb8dc02b0aff605b88ba0d13f05ee5c99c337bde
Vijendra2244 <vs230267@gmail.com>
2024-11-15 20:33:47 +0530

    changing the some flow

--- config/googleClient.config.js
+++ config/googleClient.config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const OAUTHCLIENT = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
+  "https://datanexify-assignment.onrender.com/auth/google/callback"
 );
 
 module.exports = {
